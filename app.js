db05659b103938b1890f0265d5db83cf05657210
artsoroka <githubrepository@yandex.ru>
2015-07-03 18:19:54 -0400

    user authentication

--- app.js
+++ app.js
@@ -13,8 +13,30 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs'); 
 app.set('views', __dirname + '/views'); 
 
+var authRequired = function(req,res,next){
+	var session = req.session || {}; 
+	var auth    = req.session.auth || null; 
+	if( ! auth ) 
+		return res.send('you are not logged in'); 
+	next(); 
+}
+
 app.get('/', function(req,res){
 	res.render('mainpage');  
+}); 
+
+app.get('/login', function(req,res){
+	req.session.auth = {userId: 123}; 
+	res.redirect('/mainpage'); 
+}); 
+
+app.get('/logout', function(req,res){
+	req.session.auth = null;  
+	res.redirect('/'); 
+}); 
+
+app.get('/mainpage', authRequired, function(req,res){
+	res.send('mainpage'); 
 }); 
 
 app.listen(config.app.port, function(){
