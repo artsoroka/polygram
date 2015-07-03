525af0707ce79f4aff5757212002fb19a779b426
artsoroka <githubrepository@yandex.ru>
2015-07-03 17:23:04 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express    	 = require('express');
+var session    	 = require('express-session'); 
+var bodyParser 	 = require('body-parser');  
+var cookieParser = require('cookie-parser');  
+var app        	 = express(); 
+var config	     = require('./config'); 
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(cookieParser()); 
+
+app.use(express.static(__dirname + '/public')); 
+app.set('view engine', 'ejs'); 
+
+app.get('/', function(req,res){
+	res.send('hello Polygram'); 
+}); 
+
+app.listen(config.app.port, function(){
+    console.log('Photorama is started on a port %d', config.app.port); 
+}); 
---
+++ config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	app: {
+		port: process.env.POLYGRAM_PORT || 8080
+	}
+}; 
