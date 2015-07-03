a8ebddc01e8d48bcdeaadc0bbbee7412be9c6301
artsoroka <githubrepository@yandex.ru>
2015-07-03 17:58:08 -0400

    main page layout with ejs

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.use(session(config.session))
 
 app.use(express.static(__dirname + '/public')); 
 app.set('view engine', 'ejs'); 
+app.set('views', __dirname + '/views'); 
 
 app.get('/', function(req,res){
-	res.send('hello Polygram'); 
+	res.render('mainpage');  
 }); 
 
 app.listen(config.app.port, function(){
